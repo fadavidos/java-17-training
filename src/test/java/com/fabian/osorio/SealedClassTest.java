2918caf2ed116906670a3c0f9bd57eb64d4148b5
Fabian Osorio <fadavidos@gmail.com>
2023-12-05 22:26:43 -0500

    added a test about sealed classes

---
+++ src/test/java/com/fabian/osorio/SealedClassTest.java
@@ -0,0 +1,31 @@
+package com.fabian.osorio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class SealedClassTest {
+
+    sealed class Shape permits Circle, Square, Triangle {}
+
+    final class Circle extends Shape {
+        public float radious;
+    }
+
+    final class Square extends Shape {
+        public double side = 8D;
+    }
+
+    final class Triangle extends Shape {
+        public double height, base;
+    }
+
+    // 'Other' is not allowed in the sealed hierarchy
+    // final class Other extends Shape { }
+
+    @Test
+    void usingASealedClass(){
+        Square square = new Square();
+        assertEquals(square.side, 8D);
+    }
+}
